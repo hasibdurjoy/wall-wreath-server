b8c127a4c34da3fc49ba1463c01dbac8e893286a
hasibdurjoy <hrd2571@gmail.com>
2021-11-11 15:49:11 +0600

    check

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yohkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
